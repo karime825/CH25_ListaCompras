73ff12f1fcf1368930b9af68c06a23dfb4d25716
karime <kaledon95@gmail.com>
2023-03-29 22:05:58 -0600

    se modifico

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
-// El código va aquí -> 
+let txtNombre = document.getElementById("Name");
+let txtNumber = document.getElementById("Number");
+
+let btnAgregar = document.getElementById("btnAgregar");
+let btnClear = document.getElementById("btnClear");
+
+let alertValidaciones = document.getElementById("alertValidaciones");
+let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+
+//limpiar campos
+btnClear.addEventListener("click", function(event){
+    event.preventDefault();
+    txtNombre.value="";
+    txtNumber.value="";
+
+})
+
+btnAgregar.addEventListener("click", function(event){
+    event.preventDefault();
+let lista = "Los siguientes campos deben ser llenados correctamente: <ul>";
+    if (txtNombre.value.length==0){
+        txtNombre.style.border="solid thin red";
+        lista += "<li>Se debe escribir un nombre válido</li>";
+        alertValidaciones.style.display="block";
+    }else{
+        txtNombre.style.border="";
+    } //if txtNombre
+
+    if (txtNumber.value.length==0){
+        txtNumber.style.border="solid thin red";
+        lista +="<li>Se debe escribir una cantidad válida</li>";
+        alertValidaciones.style.display="block";
+    }else{
+        txtNumber.style.border="";
+    }//if txtNumber
+    lista+="</ul>"
+    alertValidacionesTexto.insertAdjacentHTML("beforeend", lista);
+
+})
+
+txtNumber.addEventListener("blur", function(event){
+    event.preventDefault();
+    txtNumber.value = txtNumber.value.trim();
+}); // txtNumber.blur
+
+txtNombre.addEventListener("blur", function(event){
+    event.preventDefault();
+    txtNombre.value = txtNombre.value.trim();
+}); // txtNombre.blur
